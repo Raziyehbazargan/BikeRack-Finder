8f43081b0666d68d98046dd33ae43d5bb54177d4
Raziyeh Bazargan <Raziyeh@Raziyehs-MacBook-Pro.local>
2015-12-23 13:47:46 -0800

    improve the add evet listener for add pin on map on user click

--- js/app.js
+++ js/app.js
@@ -32,9 +32,7 @@ function initMap() {
   // This event listener calls addMarker() when the map is clicked.
   google.maps.event.addListener(map, 'click', function(event) {
     if (localStorage.userMarkers) {
-      var retrieveData = JSON.parse(localStorage.getItem('userMarkers'));
-      console.log(retrieveData);
-      latLongStorageArray = retrieveData;
+      latLongStorageArray = JSON.parse(localStorage.getItem('userMarkers'));
       latLongStorageArray.push({lat:event.latLng.lat() ,lng:event.latLng.lng()});
       localStorage.setItem('userMarkers', JSON.stringify(latLongStorageArray));
       addMarker(event.latLng, map); //call addMarker function
@@ -131,7 +129,6 @@ function initMap() {
   setupClickListener('changetype-geocode', ['geocode']);
 
   // ........................................................................
-
   // Add the marker at the clicked location on map, and add the label from the array of alphabetical characters.
   function addMarker(location, map) {
     var marker = new google.maps.Marker({
@@ -143,7 +140,6 @@ function initMap() {
   }
   // ........................................................................
   // show the markers on saved point by user from local storage
-
   function showMarker() {
     if (localStorage.getItem('userMarkers')) {
       var retrieveData;
@@ -151,11 +147,6 @@ function initMap() {
       for (var i = 0; i < retrieveData.length; i++) {
         var latLngLocal = new google.maps.LatLng(retrieveData[i].lat, retrieveData[i].lng);
         addMarker(latLngLocal,map)
-      //   var marker = new google.maps.Marker({
-      //   position : new google.maps.LatLng(retrieveData[i].lat, retrieveData[i].lng),
-      //   map:map,
-      //   icon:userPinImg
-      // });
     }
   }else {
     console.log('not exist');
